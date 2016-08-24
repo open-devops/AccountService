a0077d0522b8ea695a4ec17acea3d1b145274e33
Shuai Yang <shuai.yang@hpe.com>
2016-08-24 09:34:03 +0800

    此代码有编译错误，修改后上传。

--- src/main/java/com/hpe/devops/api/account/otherapi/roleservice/RoleService.java
+++ src/main/java/com/hpe/devops/api/account/otherapi/roleservice/RoleService.java
@@ -1,15 +1,10 @@
 package com.hpe.devops.api.account.otherapi.roleservice;
 
-import java.util.List;
-
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.hpe.devops.api.role.Role;
 
 @EnableFeignClients
 @FeignClient("role-service")
